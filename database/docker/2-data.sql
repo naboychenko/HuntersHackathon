80b8ab97e6a621319049098ed23740bff115e8e0
dmitry.korobovskiy <dmitry.korobovskiy@rtlabs.ru>
2019-04-21 13:00:12 +0300

    Fix insert

--- database/docker/2-data.sql
+++ database/docker/2-data.sql
@@ -1,3 +1,3 @@
 INSERT INTO patient (id, identifier, name) VALUES ('q1w2e3r4t5y6', '1q2w3e4r5t6y', 'Martin Fauler');
-INSERT INTO doctor (id, identifier, name) VALUES ('a1s2d3f4g5h6', '3d4f5g6h', 'John Nesh');
+INSERT INTO doctor (id, identifier, name, issuer, qualification) VALUES ('a1s2d3f4g5h6', '3d4f5g6h', 'John Nesh', 'z1x2c3v4b5n6', 'Wide profile');
 INSERT INTO organization(id, identifier, name) VALUES ('z1x2c3v4b5n6', '3c4v5b6n', 'Men Clinic');
